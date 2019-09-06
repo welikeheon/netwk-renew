f47cacf705df4b81a651fe2760276db0317cce6c
Heonju Lee <welikeheon@gmail.com>
2019-09-07 00:46:14 +0900

    Add ecampus, research paper

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request')
+var cheerio = require('cheerio')
+var iconv = require('iconv-lite')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
@@ -13,6 +17,27 @@ router.get('/', function(req, res, next) {
 router.get('/professor', (req, res, next) => {
     res.render('professor/professor_0100');
 });
+
+router.get('/professor/research_paper', (req, res, next) => {
+    res.render('professor/professor_0200');
+});
+
+router.get('/getpaper', (req, res, next) => {
+    var options = {
+        'uri': 'http://netwk.hannam.ac.kr/hak-non.html',
+        'encoding': 'binary'
+    }
+
+    request(options, (rror, response, body) => {
+        var result = iconv.decode(body, 'utf-8');
+        let $ = cheerio.load(result);
+
+    })
+});
+
+router.get('/e-campus', (req, res, next) => {
+    res.render('e-campus/e-campus_0100');
+})
 
 router.get('/hello', (req, res, next) => {
     res.send("Hello?")
